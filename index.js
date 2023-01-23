3f30efd1bac4734a50019ef9847a224fbcdb0756
Cata Villalonga <cata.villalonga21@gmail.com>
2023-01-23 11:26:31 +0100

    .get for milks and milks by id

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const milk_json_1 = __importDefault(require("./milk.json"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
+//get all
+app.get('/api/milks', (req, res) => {
+    return res.location('/api/milks')
+        .status(200)
+        .json(milk_json_1.default);
+});
+//get by id
+app.get('/api/milks/:id', (req, res) => {
+    const id = req.params.id;
+    const milk = milk_json_1.default.results.filter(milk => milk.id === id);
+    if (milk.length !== 0) {
+        return res.location('/api/milks/:id')
+            .status(200)
+            .json(milk);
+    }
+    return res.status(404)
+        .json({
+        error: 404,
+        msg: 'Milk not found'
+    });
+});
+//get all by type
+app.get('/api/milks/type/:type', (req, res) => {
     res.send('Express + TypeScript Server');
 });
 app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -1,14 +1,41 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import milkData from './milk.json'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+//get all
+app.get('/api/milks', (req: Request, res: Response) => {
+	return res.location('/api/milks')
+  	.status(200)
+  	.json(milkData);
 });
+
+//get by id
+app.get('/api/milks/:id', (req: Request, res: Response) => {
+	const id = req.params.id
+  const milk = milkData.results.filter(milk => milk.id === id)
+
+  if (milk.length !== 0) {
+    return res.location('/api/milks/:id')
+    .status(200)
+    .json(milk);
+  }
+  return res.status(404)
+  .json({
+    error: 404,
+    msg: 'Milk not found'
+  });
+});
+
+//get all by type
+app.get('/api/milks/type/:type', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
